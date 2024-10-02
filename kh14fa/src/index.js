a287dbe7cf50ef535d27217429dea7f43029d8d7
Park8161 <qkraudgh55@gmail.com>
2024-10-02 13:06:01 +0900

    설정 추가

--- kh14fa/src/index.js
+++ kh14fa/src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// import design
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootswatch/dist/spacelab/bootstrap.min.css';
+import 'bootstrap';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
