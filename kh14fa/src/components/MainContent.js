53954ced6d29c577de411ac71ef8ca4a55b4ff03
Park8161 <qkraudgh55@gmail.com>
2024-10-02 14:19:43 +0900

    MainContent, PageNotFound 추가

---
+++ kh14fa/src/components/MainContent.js
@@ -0,0 +1,55 @@
+// import
+import {useState, useMemo} from "react";
+import {Routes, Route} from "react-router";
+import Home from './Home';
+// import MemberComplexSearch from "./search/MemberComplexSearch";
+// import Search from "./search/Search";
+// import MemberLogin from "./member/MemberLogin";
+
+// component
+const MainContent = ()=>{
+    // state
+
+    // effect
+
+    // memo
+
+    // view
+    return (
+        <>
+            {/* 컨테이너 */}
+            <div className="container-fluid">
+
+                {/* 메인 폭 조절 영역 */}
+                <div className="row my-5 pt-5">
+                    <div className="col-sm-10 offset-sm-1">
+                        
+                        {/* 
+                            주소에 따라 배치될 화면에 대한 설정(라우터 설정) 
+                            위에서 아래로 검색한다 : 사용빈도가 높을수록 위쪽에 두는 것이 좋다
+                        */}
+                        <Routes>
+                            <Route exact path="/" element={<Home/>} /> 
+                            {/* exact path : 정확하게 일치할때만 나오게 할 수 있다 */}
+                            {/* 경로변수를 사용할 경우 콜론과 이름을 합쳐 변수명으로 지정 ("/book/detail/{bookID}") */}
+                            
+                            {/* 변경 : 중첩 라우팅 */}
+                            <Route path="/search/*" element={<Search/>} />
+
+                            {/* 회원 로그인 */}
+                            <Route path="/member/login" element={<MemberLogin/>} />
+
+                            {/* 나머지 경로(*) 패턴을 지정해서 미 지정된 페이지를 모두 연결 할 수 있다 */}
+                            <Route path="*" element={<PageNotFound/>} />
+                        </Routes>
+
+                    </div>
+                </div>
+
+            </div>
+        </>
+    );
+}
+
+// export
+export default MainContent;
