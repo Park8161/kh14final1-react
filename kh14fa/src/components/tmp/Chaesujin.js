5258c912a8eb3c3dd27e0aa7d8010f6400afa671
Park8161 <qkraudgh55@gmail.com>
2024-10-17 13:01:32 +0900

    팀플 중 충돌 방지 임시 라우팅 생성

---
+++ kh14fa/src/components/tmp/Chaesujin.js
@@ -0,0 +1,13 @@
+import { Routes } from "react-router";
+
+// 채수진
+const Chaesujin = ()=>{
+    return (
+        <>
+        <Routes>
+            {/* <Route path="/autocomplete" element={<AutoComplete/>} /> */}
+        </Routes>
+        </>
+    );
+};
+export default Chaesujin;
---
+++ kh14fa/src/components/tmp/Gykim94.js
@@ -0,0 +1,13 @@
+import { Routes } from "react-router";
+
+// 김기영
+const Gykim94 = ()=>{
+    return (
+        <>
+        <Routes>
+            {/* <Route path="/autocomplete" element={<AutoComplete/>} /> */}
+        </Routes>
+        </>
+    );
+};
+export default Gykim94;
