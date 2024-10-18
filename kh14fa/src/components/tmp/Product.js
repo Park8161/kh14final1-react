b4b33915e9f554a7801b4c075f96be49af450e8a
ChaeSuJin <univer7519@naver.com>
2024-10-18 21:41:21 +0900

    상세페이지 구현중

--- kh14fa/src/components/tmp/Product.js
+++ kh14fa/src/components/tmp/Product.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router";
 import ProductInsert from "../product/ProductInsert";
 import ProductList from "../product/ProductList";
+import ProductDetail from "../product/ProductDetail";
 
 const Product = ()=>{
     return (
@@ -9,7 +10,7 @@ const Product = ()=>{
             {/* <Route path="/autocomplete" element={<AutoComplete/>} /> */}
             <Route path="/tempInsert" element={<ProductInsert/>}></Route>
             <Route path="/list" element={<ProductList/>}></Route>
-            <Route />
+            <Route path="/detail/:productNo" element={<ProductDetail/>}> </Route>
         </Routes>
         </>
     );
