947044fe80bd40ea3c76adfa5a8f7f39ebeff582
aldskaldsk <aldskaldsk@gmail.com>
2024-10-22 18:12:01 +0900

    결제 큐알 까지

--- kh14fa/src/components/tmp/Pay.js
+++ kh14fa/src/components/tmp/Pay.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router";
+import ChooseOption from "../pay/ChooseOption";
 
 const Pay = ()=>{
     return (
         <>
         <Routes>
             {/* <Route path="/autocomplete" element={<AutoComplete/>} /> */}
+            <Route path="/chooseoption/:productNo" element={<ChooseOption/>} /> 
             <Route />
         </Routes>
         </>
