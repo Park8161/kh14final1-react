12c88d7bda9f509edd9f89d9c27fac629dc432e8
Park8161 <qkraudgh55@gmail.com>
2024-11-02 02:24:20 +0900

    목록 데이터 재조정및 디자인 재조정

--- kh14fa/src/components/admin/Product.js
+++ kh14fa/src/components/admin/Product.js
@@ -5,6 +5,8 @@ import { useNavigate } from "react-router";
 import { FaMagnifyingGlass, FaPlus } from "react-icons/fa6";
 import { NavLink } from "react-router-dom";
 import { toast } from 'react-toastify';
+import moment from 'moment';
+import "moment/locale/ko"; // moment에 한국어 정보 불러오기
 
 const Product = ()=>{
     // navigate
@@ -18,10 +20,12 @@ const Product = ()=>{
     });
     const [page, setPage] = useState(1); 
     const [pageSize, setPageSize] = useState(10); 
+    const [category, setCategory] = useState([]);
 
     // effect
     useEffect(() => {
         loadProductList();
+        loadCategory();
     }, []);
 
     // callback
@@ -40,6 +44,12 @@ const Product = ()=>{
         const response = await axios.get(`/product/list/${encodeURIComponent(input.column)}/${encodeURIComponent(input.keyword)}`);
         setProductList(response.data);
     },[input]);
+
+    // 카테고리 리스트
+    const loadCategory = useCallback(async()=>{
+        const response = await axios.get("/admin/category/listP");
+        setCategory(response.data);
+    },[category]);
 
     const changeInput = useCallback((e)=>{
         setInput({
@@ -74,6 +84,15 @@ const Product = ()=>{
         }
     },[]);
 
+    //시간 계산 함수 (매개변수)
+	const timeCalculate = useCallback((productTime)=>{
+		const date = moment.utc(productTime).tz('Asia/Seoul'); // 한국 시간으로 변환
+
+		const nowDate = moment().tz('Asia/Seoul'); // 현재 시간을 한국 시간으로 설정
+
+        return date;
+    },[]);
+
     // view
     return (
         <>
@@ -82,18 +101,18 @@ const Product = ()=>{
                     {/* 검색창 */}
                     <div className="row mt-4">
                         <div className="col-6 offset-3">
-                            <div className="input-group w-auto">
-                                <select type="search" className="form-select bg-white" 
-                                        name="column" value={input.column} onChange={changeInput}>
+                            <div className="input-group">
+                                <select name="column" className="form-select w-auto" 
+                                        value={input.column} onChange={changeInput}>
                                     <option value="">선택</option>
                                     <option value="product_name">상품명</option>
                                     <option value="product_member">판매자</option>
+                                    <option value="category_name">카테고리</option>
                                 </select>
-                                <input type="search" className="form-control w-auto bg-white" 
+                                <input type="search" className="form-control w-auto" placeholder="검색어 입력"
                                         name="keyword" value={input.keyword} onChange={changeInput}/>
-                                <button type="button" className="btn btn-secondary d-flex justify-content-center align-items-center" onClick={loadSearchList}>
-                                    <FaMagnifyingGlass />
-                                    검색
+                                <button type="button" className="btn btn-secondary" onClick={loadSearchList}>
+                                    <FaMagnifyingGlass /> 검색
                                 </button>
                             </div>
                         </div>
@@ -103,11 +122,12 @@ const Product = ()=>{
                     <div className="row mt-4">
                         <div className="col-8 offset-2">
                             {/* <div className="table-responsive"> */}
-                                <table className="table text-truncate text-center">
+                                <table className="table border table-hover table-no-borders text-center">
                                     <thead>
                                         <tr>
                                             <th>상품번호</th>
                                             <th>상품명</th>
+                                            <th>카테고리</th>
                                             <th>판매자</th>
                                             <th>판매가</th>
                                             <th>수량</th>
@@ -120,13 +140,14 @@ const Product = ()=>{
                                         {getPagedNotice().map((product)=>(
                                         <tr key={product.productNo}>
                                             <td>{product.productNo}</td>
-                                            <td className="text-truncate" onClick={e=>navigate("/product/detail/"+product.productNo)} style={{cursor:"pointer"}}>
+                                            <td className="table-truncate" onClick={e=>navigate("/product/detail/"+product.productNo)} style={{cursor:"pointer"}}>
                                                 {product.productName}
                                             </td>
+                                            <td>{category.filter(cat => cat.categoryNo === product.productCategory)[0]?.categoryName}</td>
                                             <td>{product.productMember}</td>
                                             <td>{product.productPrice}</td>
                                             <td>{product.productQty}</td>
-                                            <td>{product.productDate}</td>
+                                            <td>{moment(product.productDate).format("YYYY-MM-DD")}</td>
                                             <td>{product.productState}</td>
                                             <td>
                                                 <button className="btn btn-secondary btn-sm" onClick={e=>changeHoldOff(product,"판매중")} disabled={product.productState === "판매중"}>
