f93cffc67e6cde72869135cbed140a213d008086
Park8161 <qkraudgh55@gmail.com>
2024-11-04 19:22:03 +0900

    관리메뉴 이동 및 홈화면 변경

--- kh14fa/src/components/Home.js
+++ kh14fa/src/components/Home.js
@@ -1,15 +1,19 @@
 // import
+import { useEffect } from "react";
 import { useNavigate } from "react-router";
 
 // component
 const Home = () => {
     // navigate
     const navigate = useNavigate();
+
+    useEffect(()=>{
+        navigate("/product/list");
+    },[]);
     
     // view
     return (
         <>
-        {navigate("/product/list")};
         </>
     );
 }
