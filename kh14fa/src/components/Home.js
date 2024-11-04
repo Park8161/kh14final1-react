21a38eace8510518d915fc3aa70ee95187a87f1a
Park8161 <qkraudgh55@gmail.com>
2024-11-04 14:27:25 +0900

    Footer 작업 진행 중

--- kh14fa/src/components/Home.js
+++ kh14fa/src/components/Home.js
@@ -56,100 +56,106 @@ const Home = () => {
     // view
     return (
         <>
-          <div className="container mt-4 grid grid-cols-1 gap-4">
-            <div className="flex items-center">
-                <h3 className="text-lg md:text-xl lg:text-2xl 2xl:text-3xl xl:leading-10 font-bold text-heading">
-                    진행중인 이벤트
-                </h3>
-            </div>
+        <div className="row">
+            <div className="col">
 
-            {/* 이벤트 배너 */}
-            <div className="container mt-4">
-                <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
-                    {/* <div className="carousel-inner">
-                        <div className="carousel-item active">
-                            <div className="row">
-                                {displayedBanners.map((banner, index) => (
-                                    <div className="col" key={index}>
-                                        <img src={banner[0]} className="d-block w-100" alt={banner[1]} style={{ width: '300px' , height: '300px', objectFit: 'contain', margin:'0 5px'}} />
-                                    </div>
-                                ))}
+            <div className="container mt-4 grid grid-cols-1 gap-4">
+                <div className="flex items-center">
+                    <h3 className="text-lg md:text-xl lg:text-2xl 2xl:text-3xl xl:leading-10 font-bold text-heading">
+                        진행중인 이벤트
+                    </h3>
+                </div>
+
+                {/* 이벤트 배너 */}
+                <div className="container mt-4">
+                    <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
+                        {/* <div className="carousel-inner">
+                            <div className="carousel-item active">
+                                <div className="row">
+                                    {displayedBanners.map((banner, index) => (
+                                        <div className="col" key={index}>
+                                            <img src={banner[0]} className="d-block w-100" alt={banner[1]} style={{ width: '300px' , height: '300px', objectFit: 'contain', margin:'0 5px'}} />
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        </div> */}
+                        <div className="carousel-inner">
+                            <div className="carousel-item active">
+                                <div className="row">
+                                    {displayedBanners.map((banner, index) => (
+                                        <div className="col" key={index}>
+                                            <img src={`${process.env.REACT_APP_BASE_URL}/attach/download/${banner.attachment}`} 
+                                                    className="d-block w-100" alt={banner.title} onClick={e=>BannerClick(banner.noticeNo)}
+                                                    style={{ 
+                                                        width: '250px' ,
+                                                        height: '300px', 
+                                                        objectFit: 'fill', 
+                                                        margin:'0 1px',
+                                                        padding: '1px'
+                                                    }} />
+                                        </div>
+                                    ))}
+                                </div>
                             </div>
                         </div>
-                    </div> */}
-                    <div className="carousel-inner">
-                        <div className="carousel-item active">
-                            <div className="row">
-                                {displayedBanners.map((banner, index) => (
-                                    <div className="col" key={index}>
-                                        <img src={`${process.env.REACT_APP_BASE_URL}/attach/download/${banner.attachment}`} 
-                                                className="d-block w-100" alt={banner.title} onClick={e=>BannerClick(banner.noticeNo)}
-                                                style={{ 
-                                                    width: '250px' ,
-                                                    height: '300px', 
-                                                    objectFit: 'fill', 
-                                                    margin:'0 1px',
-                                                    padding: '1px'
-                                                }} />
-                                    </div>
-                                ))}
-                            </div>
+                        {/* 이전 버튼 (<) */}
+                        <button className="carousel-control-prev" type="button" onClick={prevBanner}
+                                    data-bs-slide="prev"
+                                    style={{
+                                        position: 'absolute'  ,
+                                        left: '-70px',
+                                        top: '50%',
+                                        transform: 'translateY(-50%)',
+                                        zIndex: '1',
+                                        border: 'none'
+                                    }}>
+                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Previous</span>
+                        </button>                                    
+                        {/* 다음 버튼 (>) */}
+                        <button className="carousel-control-next" type="button" onClick={nextBanner}
+                                    data-bs-slide="next"
+                                    style={{
+                                        position: 'absolute'  ,
+                                        right: '-70px',
+                                        top: '50%',
+                                        transform: 'translateY(-50%)',
+                                        zIndex: '1',
+                                        border: 'none'
+                                    }}>
+                                    
+                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Next</span>
+                        </button>
+                        
+                        
+                        {/* 인디케이터 추가 */}
+                        <div className="carousel-indicators">
+                            {[...Array(Math.ceil(bannerList.length / 3))].map((_, index) => (
+                                <button
+                                    key={index}
+                                    type="button"
+                                    className={currentIndex === index ? "active" : ""}
+                                    aria-current={currentIndex === index ? "true" : "false"}
+                                    onClick={() => setCurrentIndex(index)}
+                                    style={{
+                                        borderRadius: '50%',
+                                        width: '10px',
+                                        height: '10px',
+                                        margin: '2px',
+                                        backgroundColor: currentIndex === index ? 'black' : 'lightgray', // 색상 변경
+                                        border: 'none', // 기본 테두리 제거
+                                    }}
+                                ></button>
+                            ))}
                         </div>
-                    </div>
-                    {/* 이전 버튼 (<) */}
-                    <button className="carousel-control-prev" type="button" onClick={prevBanner}
-                                data-bs-slide="prev"
-                                style={{
-                                    position: 'absolute'  ,
-                                    left: '-70px',
-                                    top: '50%',
-                                    transform: 'translateY(-50%)',
-                                    zIndex: '1',
-                                    border: 'none'
-                                }}>
-                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Previous</span>
-                    </button>                                    
-                    {/* 다음 버튼 (>) */}
-                    <button className="carousel-control-next" type="button" onClick={nextBanner}
-                                data-bs-slide="next"
-                                style={{
-                                    position: 'absolute'  ,
-                                    right: '-70px',
-                                    top: '50%',
-                                    transform: 'translateY(-50%)',
-                                    zIndex: '1',
-                                    border: 'none'
-                                }}>
-                                
-                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Next</span>
-                    </button>
-                    
-                    
-                    {/* 인디케이터 추가 */}
-                    <div className="carousel-indicators">
-                        {[...Array(Math.ceil(bannerList.length / 3))].map((_, index) => (
-                            <button
-                                key={index}
-                                type="button"
-                                className={currentIndex === index ? "active" : ""}
-                                aria-current={currentIndex === index ? "true" : "false"}
-                                onClick={() => setCurrentIndex(index)}
-                                style={{
-                                    borderRadius: '50%',
-                                    width: '10px',
-                                    height: '10px',
-                                    margin: '2px',
-                                    backgroundColor: currentIndex === index ? 'black' : 'lightgray', // 색상 변경
-                                    border: 'none', // 기본 테두리 제거
-                                }}
-                            ></button>
-                        ))}
                     </div>
                 </div>
+                </div>
+
             </div>
-            </div>
+        </div>
         </>
     );
 }
